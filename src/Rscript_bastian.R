d0544aa8d584b71adce4228191a9e9646d77069f
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2018-11-12 14:01:03 -0500

    Created assumptions section, added equations for null and alt.

--- src/Rscript_bastian.R
+++ src/Rscript_bastian.R
@@ -14,3 +14,24 @@ bastian <- read_csv("../data/Bastian Jetten and Ferris 2014 Experiment 1.csv")
 
 # Find variables of interest
 names(bastian)
+
+# Select & rename variables of interest
+bastian_clean <- bastian %>%
+  select(
+    condition = CONDITION,
+    group101:task_unpleasantness,
+    threat_mean = Threat_MEAN,
+    challenge_mean = Challenge_MEAN,
+    pos_affect = Pos_PANAS,
+    neg_affect = Neg_PANAS
+  )
+
+####### ANALYZE #######
+
+### Independent t-Test of Pain ###
+
+### Independent t-Test of Affect ###
+
+### Independent t-Test of Challenge ###
+
+### One-way ANOVA ###
